95958cb0d54bdfd7012093e74c2f034edf0a3251
Bryan Ariel <saariel115@gmail.com>
2021-10-24 20:04:30 -0600

    La otra semana retomo angular, estoy con react ahora

---
+++ 00-CursoAngular/TiendaVirtual/src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// OJO TODO  BOTON, CLASE X DE MATERIAL A USAR LO EXPORTAREMOS AQUI
+@NgModule({
+  exports: [MatToolbarModule]
+})
+
+export class MaterialModule {
+
+}
