520487f33a01870597abdfd709df6dbdda3174f3
Bryan Anariba <saariel115@gmail.com>
2023-06-06 21:31:04 -0600

    Finalizando Seccion Dos

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+  public deletedHero?: string;
   public heroes: string[] = ['Spiderman', 'Hulk', 'Iromman', 'She Hulk'];
+
+  onDeleteHero (): void {
+    this.deletedHero = this.heroes.pop();
+    console.log({ hero: this.deletedHero });
+  }
 }
